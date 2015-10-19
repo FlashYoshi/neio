985ceca34804a5f2f746bf06710544c3b15fd72c
Titouan Vervack <tivervac@gmail.com>
2015-10-19 20:47:11 +0200

    Added containers

---
+++ thesis/src/main/java/be/ugent/thesis/container/Document.java
@@ -0,0 +1,7 @@
+package be.ugent.thesis.container;
+
+/**
+ * @author Titouan Vervack
+ */
+public class Document extends Root {
+}
---
+++ thesis/src/main/java/be/ugent/thesis/container/Slideshow.java
@@ -0,0 +1,7 @@
+package be.ugent.thesis.container;
+
+/**
+ * @author Titouan Vervack
+ */
+public class Slideshow extends Root {
+}
