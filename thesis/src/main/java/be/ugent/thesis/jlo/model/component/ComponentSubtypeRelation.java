709a47524db0a1cbad1f6d3d74124fca23148a74
Titouan Vervack <tivervac@gmail.com>
2015-11-04 10:24:34 +0100

    Got jlo working

---
+++ thesis/src/main/java/be/ugent/thesis/jlo/model/component/ComponentSubtypeRelation.java
@@ -0,0 +1,92 @@
+package be.ugent.thesis.jlo.model.component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.aikodi.chameleon.core.declaration.Declaration;
+import org.aikodi.chameleon.core.element.Element;
+import org.aikodi.chameleon.core.lookup.DeclarationSelector;
+import org.aikodi.chameleon.core.lookup.LookupException;
+import org.aikodi.chameleon.core.lookup.SelectionResult;
+import org.aikodi.chameleon.oo.member.Member;
+import org.aikodi.chameleon.oo.member.MemberRelationSelector;
+import org.aikodi.chameleon.oo.type.Type;
+import org.aikodi.chameleon.oo.type.TypeReference;
+import org.aikodi.chameleon.oo.type.inheritance.SubtypeRelation;
+
+/**
+ * This class represents the subtype relation between a component type, and the declared type of
+ * the component. This subtype relation incorporates the inherited members into the component type.
+ * @author Marko van Dooren
+ *
+ */
+public class ComponentSubtypeRelation extends IncorporatingSubtypeRelation {
+	
+	public ComponentSubtypeRelation() {
+	}
+	
+	@Override
+	public TypeReference superClassReference() {
+		return nearestAncestor(Subobject.class).componentTypeReference();
+	}
+	
+	public Type componentType() {
+		return nearestAncestor(Type.class);
+	}
+	
+	/**
+	 * A subtype component relation equals another element if the other element
+	 * is a subtype relation and its parent is the component type.
+	 */
+	@Override
+	public boolean uniSameAs(Element other) throws LookupException {
+		//FIXME: the type of the instanceof should probably be changed to ComponentSubtypeRelation.
+		return other instanceof SubtypeRelation && other.parent() == componentType();
+	}
+
+	private <M extends Member> List<SelectionResult<M>> incorporated(List<? extends SelectionResult<M>> tmp) throws LookupException {
+		Subobject componentRelation = componentType().nearestAncestor(Subobject.class);
+		List<SelectionResult<M>> result = new ArrayList<>();
+		for(SelectionResult<M> r:tmp) {
+			Declaration decl = r.template();
+			SelectionResult<M> inc = r.updatedTo(componentRelation.incorporatedIntoComponentType((Member)decl));
+			result.add(inc);
+		}
+		return result;
+	}
+	
+
+	@Override
+	public <M extends Member> List<SelectionResult<M>> potentiallyInheritedMembers(DeclarationSelector<M> selector) throws LookupException {
+		List<? extends SelectionResult<M>> potentiallyInheritedMembers = super.potentiallyInheritedMembers(selector);
+		return incorporated(potentiallyInheritedMembers);
+	}
+
+	@Override
+	public <M extends Member> List<M> potentiallyInheritedMembers(Class<M> kind) throws LookupException {
+		List<M> potentiallyInheritedMembers = super.potentiallyInheritedMembers(kind);
+		return (List)incorporated((List)potentiallyInheritedMembers);
+	}
+
+	@Override
+	public List<Member> potentiallyInheritedMembers() throws LookupException {
+		List<Member> potentiallyInheritedMembers = super.potentiallyInheritedMembers();
+		return (List)incorporated((List)potentiallyInheritedMembers);
+	}
+
+	@Override
+	public <D extends Member> List<D> membersDirectlyOverriddenBy(MemberRelationSelector<D> selector) throws LookupException {
+		return (List)incorporated((List)super.membersDirectlyOverriddenBy(selector));
+	}
+
+	@Override
+	public <D extends Member> List<D> membersDirectlyAliasedBy(MemberRelationSelector<D> selector) throws LookupException {
+		return new ArrayList<D>();
+	}
+	
+	protected SubtypeRelation cloneSelf() {
+		return new ComponentSubtypeRelation();
+	}
+
+
+}
---
+++ thesis/src/main/java/be/ugent/thesis/jlo/model/type/TypeMemberDeclarator.java
@@ -0,0 +1,130 @@
+package be.ugent.thesis.jlo.model.type;
+
+import java.util.List;
+
+import org.aikodi.chameleon.core.declaration.Signature;
+import org.aikodi.chameleon.core.declaration.SimpleNameSignature;
+import org.aikodi.chameleon.core.element.Element;
+import org.aikodi.chameleon.core.lookup.LookupContext;
+import org.aikodi.chameleon.core.lookup.LookupException;
+import org.aikodi.chameleon.core.modifier.ElementWithModifiersImpl;
+import org.aikodi.chameleon.oo.member.Member;
+import org.aikodi.chameleon.oo.type.Type;
+import org.aikodi.chameleon.oo.type.TypeFixer;
+import org.aikodi.chameleon.oo.type.generics.EqualityConstraint;
+import org.aikodi.chameleon.oo.type.generics.FormalTypeParameter;
+import org.aikodi.chameleon.oo.type.generics.TypeConstraint;
+import org.aikodi.chameleon.oo.type.generics.TypeParameter;
+import org.aikodi.chameleon.oo.type.generics.TypeParameterFixer;
+import org.aikodi.chameleon.util.Lists;
+import org.aikodi.chameleon.util.association.Single;
+
+public class TypeMemberDeclarator extends ElementWithModifiersImpl implements Member {
+
+  private final class TypeMemberParameterFixer extends
+  TypeParameterFixer {
+
+    private Single<TypeParameter> _parameter = new Single<>(this);
+
+    public TypeMemberParameterFixer(TypeParameter parameter) {
+      set(_parameter,parameter);
+    }
+
+    @Override
+    protected List<TypeParameter> parameters() throws LookupException {
+      List<TypeMemberDeclarator> members = nearestAncestor(Type.class).members(TypeMemberDeclarator.class);
+      List<TypeParameter> result = Lists.create(members.size());
+      result.add(parameter());
+      // FIXME inefficient. Just store an immutable list.
+      members.forEach(m -> {
+        if(m != TypeMemberDeclarator.this) {
+          TypeParameter parameter = clone(m.parameter());
+          parameter.setUniParent(this);
+          result.add(parameter);
+        }
+      });
+      return result;
+    }
+
+    /**
+     * @return
+     */
+    private TypeParameter parameter() {
+      return _parameter.getOtherEnd();
+    }
+
+    @Override
+    protected Element cloneSelf() {
+      return new TypeMemberParameterFixer(null);
+    }
+  }
+
+  public TypeMemberDeclarator(TypeParameter parameter) {
+    set(_fixer, new TypeMemberParameterFixer(parameter));
+  }
+
+//  public TypeMemberDeclarator(SimpleNameSignature signature) {
+//    setSignature(signature);
+//  }
+//
+  /**
+   * A type variable introduces a type parameter.
+   */
+  @Override
+  public List<? extends Member> getIntroducedMembers() {
+    return Lists.create(this);
+  }
+
+  @Override
+  public LookupContext targetContext() throws LookupException {
+    return selectionDeclaration().targetContext();
+  }
+
+  @Override
+  public Type selectionDeclaration() throws LookupException {
+    //		TypeParameter parameter = createParameter();
+    //		TypeMemberParameterFixer fixer = new TypeMemberParameterFixer();
+    //		parameter.setUniParent(fixer);
+    return parameter().selectionDeclaration();
+  }
+
+  private Single<TypeMemberParameterFixer> _fixer = new Single(this);
+
+  public TypeParameter parameter() {
+    return _fixer.getOtherEnd().parameter();
+  }
+
+  @Override
+  public Signature signature() {
+    return parameter().signature();
+  }
+  
+  @Override
+  public void setSignature(Signature signature) {
+    parameter().setSignature(signature);
+  }
+
+//  private Single<Signature> _signature = new Single<>(this);
+//
+//  private Single<TypeConstraint> _constraint = new Single<>(this);
+//
+//  public TypeConstraint constraint() {
+//    return _constraint.getOtherEnd();
+//  }
+//
+//  public void setConstraint(TypeConstraint constraint) {
+//    set(_constraint, constraint);
+//  }
+
+  @Override
+  protected Element cloneSelf() {
+    return new TypeMemberDeclarator(null);
+  }
+
+  public boolean contains(TypeMemberDeclarator s, TypeFixer trace) throws LookupException {
+    return selectionDeclaration().contains(s.selectionDeclaration(), trace);
+  }
+
+
+
+}
