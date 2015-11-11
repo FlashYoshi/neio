2456ce00007551a107b608675de44ae9bf234754
Titouan Vervack <tivervac@gmail.com>
2015-11-11 14:47:01 +0100

    Made extensions and fields work

--- neio/src/main/java/be/ugent/neio/parsing/ClassConverter.java
+++ neio/src/main/java/be/ugent/neio/parsing/ClassConverter.java
@@ -1,22 +1,24 @@
 package be.ugent.neio.parsing;
 
-import be.kuleuven.cs.distrinet.jnome.core.type.RegularJavaType;
 import be.kuleuven.cs.distrinet.jnome.workspace.JavaView;
+import be.ugent.neio.industry.NeioFactory;
 import be.ugent.neio.language.Neio;
 import be.ugent.neio.parsing.ClassParser.*;
 import be.ugent.neio.util.Keywords;
 import org.aikodi.chameleon.core.document.Document;
-import org.aikodi.chameleon.oo.expression.MethodInvocation;
+import org.aikodi.chameleon.core.factory.Factory;
+import org.aikodi.chameleon.core.namespacedeclaration.NamespaceDeclaration;
 import org.aikodi.chameleon.oo.method.Method;
 import org.aikodi.chameleon.oo.method.SimpleNameMethodHeader;
+import org.aikodi.chameleon.oo.plugin.ObjectOrientedFactory;
 import org.aikodi.chameleon.oo.type.BasicTypeReference;
 import org.aikodi.chameleon.oo.type.Type;
 import org.aikodi.chameleon.oo.type.TypeReference;
+import org.aikodi.chameleon.oo.type.inheritance.InheritanceRelation;
 import org.aikodi.chameleon.oo.type.inheritance.SubtypeRelation;
+import org.aikodi.chameleon.oo.variable.MemberVariable;
 import org.aikodi.chameleon.oo.variable.RegularMemberVariable;
 import org.aikodi.chameleon.support.member.simplename.method.NormalMethod;
-import org.aikodi.chameleon.support.member.simplename.method.RegularMethodInvocation;
-import org.antlr.v4.runtime.TokenStream;
 
 import static be.ugent.neio.util.Keywords.CLASS;
 import static be.ugent.neio.util.Keywords.INTERFACE;
@@ -34,6 +36,14 @@ public class ClassConverter extends ClassParserBaseVisitor<Object> {
         this.document = document;
         this.view = view;
         this.neio = view.language(Neio.class);
+    }
+
+    protected Factory factory() {
+        return neio.plugin(Factory.class);
+    }
+
+    protected NeioFactory ooFactory() {
+        return (NeioFactory) neio.plugin(ObjectOrientedFactory.class);
     }
 
     public Object visitDocument(DocumentContext ctx) {
@@ -58,30 +68,44 @@ public class ClassConverter extends ClassParserBaseVisitor<Object> {
 
     private void visitClass(DocumentContext ctx, String klassName) {
         ClassBodyContext body = ctx.body().classBody();
-        Type klass = new RegularJavaType(klassName);
+        NamespaceDeclaration ns = factory().createRootNamespaceDeclaration();
+        Type klass = ooFactory().createRegularType(klassName);
         visitExtensions(body, klass);
         visitFields(body, klass);
         visitMethods(body, klass);
+        ns.add(klass);
+        document.add(ns);
     }
 
     private void visitExtensions(ClassBodyContext body, Type type) {
         for (ExtensionContext extension : body.extension()) {
-            // Do not need double check because if they are both null the method would not have been called
-            if (extension.IMPLEMENTS() != null) {
-                System.out.println(extension.IMPLEMENTS().getText() + " " + extension.chain().getText());
-            } else {
-                System.out.println(extension.EXTENDS().getText() + " " + extension.chain().getText());
-            }
-            type.addInheritanceRelation(new SubtypeRelation((TypeReference) visit(extension)));
+            System.out.println(extension.chain().getText());
+            type.addInheritanceRelation(visitExtension(extension));
         }
+    }
+
+    @Override
+    public InheritanceRelation visitExtension(ExtensionContext ctx) {
+        // Ignore the namespace for now, it does not really matter at this point
+        String ignoredNS = ctx.chain().getChild(ctx.chain().getChildCount() - 1).getText();
+        TypeReference ref = ooFactory().createTypeReference(ignoredNS);
+
+        return ooFactory().createSubtypeRelation(ref);
     }
 
     private void visitFields(ClassBodyContext body, Type type) {
         for (FieldContext field : body.field()) {
             System.out.println(field.var().fieldName().getText() + " " + field.var().CAMEL_CASE().getText() + ";");
-            type.add(new RegularMemberVariable(field.var().CAMEL_CASE().getText(), (TypeReference) visit(field.var().fieldName())));
+            type.add(visitField(field));
         }
-        System.out.println();
+    }
+
+    @Override
+    public MemberVariable visitField(FieldContext ctx) {
+        String name = ctx.var().CAMEL_CASE().getText();
+        TypeReference type = ooFactory().createTypeReference(ctx.var().fieldName().getText());
+
+        return ooFactory().createMemberVariable(name, type);
     }
 
     private void visitMethods(ClassBodyContext body, Type type) {
