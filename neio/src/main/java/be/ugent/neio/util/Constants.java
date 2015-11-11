9654afc731280dd97b07b04141c367fa26ab42d2
Titouan Vervack <tivervac@gmail.com>
2015-11-11 01:49:30 +0100

    Added auto generated files directory

--- neio/src/main/java/be/ugent/neio/util/Constants.java
+++ neio/src/main/java/be/ugent/neio/util/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
     public static final String EXTENSION = "no";
     public static final String VERSION = "0.5";
     public static final String NEIO_HOME = "../examples/" + VERSION + "/neio";
+    public static final String AUTO_GEN_DIR = "./gen";
 }
