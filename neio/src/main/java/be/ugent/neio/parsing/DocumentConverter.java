5bc3c1763510ffc133eb8335d6d500b4311265d6
Titouan Vervack <tivervac@gmail.com>
2015-11-17 00:07:48 +0100

    Java auto generation of chapters

--- neio/src/main/java/be/ugent/neio/parsing/DocumentConverter.java
+++ neio/src/main/java/be/ugent/neio/parsing/DocumentConverter.java
@@ -4,25 +4,35 @@ import be.kuleuven.cs.distrinet.jnome.workspace.JavaView;
 import be.ugent.neio.industry.NeioExpressionFactory;
 import be.ugent.neio.industry.NeioFactory;
 import be.ugent.neio.language.Neio;
+import be.ugent.neio.language.Nested;
 import be.ugent.neio.parsing.DocumentParser.ContentContext;
 import be.ugent.neio.parsing.DocumentParser.DocumentContext;
 import be.ugent.neio.parsing.DocumentParser.SentenceContext;
 import org.aikodi.chameleon.core.document.Document;
 import org.aikodi.chameleon.core.factory.Factory;
-import org.aikodi.chameleon.core.namespace.Namespace;
+import org.aikodi.chameleon.core.lookup.LookupException;
+import org.aikodi.chameleon.core.modifier.Modifier;
 import org.aikodi.chameleon.core.namespace.NamespaceReference;
-import org.aikodi.chameleon.core.namespace.RegularNamespace;
 import org.aikodi.chameleon.core.namespacedeclaration.NamespaceDeclaration;
+import org.aikodi.chameleon.oo.expression.Expression;
 import org.aikodi.chameleon.oo.expression.ExpressionFactory;
 import org.aikodi.chameleon.oo.method.Method;
 import org.aikodi.chameleon.oo.plugin.ObjectOrientedFactory;
 import org.aikodi.chameleon.oo.statement.Block;
 import org.aikodi.chameleon.oo.statement.Statement;
 import org.aikodi.chameleon.oo.type.Type;
+import org.aikodi.chameleon.oo.type.TypeReference;
 import org.aikodi.chameleon.oo.variable.FormalParameter;
 import org.aikodi.chameleon.support.modifier.Public;
 import org.aikodi.chameleon.support.modifier.Static;
 import org.antlr.v4.runtime.tree.TerminalNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 
 /**
@@ -34,12 +44,18 @@ public class DocumentConverter extends DocumentParserBaseVisitor<Object> {
     private final Neio neio;
     private final Block block;
     private final String name;
+    private final Stack<Variable> vars;
+    private final JavaView view;
+    private int varCount = 0;
+    private static final String VAR_NAME = "var";
 
     public DocumentConverter(Document document, JavaView view, String name) {
         this.document = document;
+        this.view = view;
         this.neio = view.language(Neio.class);
         this.block = ooFactory().createBlock();
         this.name = name;
+        this.vars = new Stack<>();
     }
 
     protected Factory factory() {
@@ -64,7 +80,6 @@ public class DocumentConverter extends DocumentParserBaseVisitor<Object> {
     }
 
     private void fillDocument() {
-        //NamespaceDeclaration ns = document.namespaceDeclarations().get(0);
         NamespaceDeclaration ns = factory().createNamespaceDeclaration(new NamespaceReference("be.ugent"));
         Type type = ooFactory().createRegularType(name);
         type.addModifier(new Public());
@@ -83,11 +98,17 @@ public class DocumentConverter extends DocumentParserBaseVisitor<Object> {
         String header = ctx.HEADER().getText();
         // Strip the brackets
         String documentType = header.substring(1, header.length() - 1);
-        String name = documentType.toLowerCase();
-        Statement v = ooFactory().createNewDeclaration(documentType, name, expressionFactory().createNewExpression(documentType));
+        String name = getVarName();
+        TypeReference type = ooFactory().createTypeReference(documentType);
+        Statement s = ooFactory().createLocalVariable(type, name, expressionFactory().createNewExpression(documentType));
+        vars.push(new Variable(name, type));
 
         System.out.println(ctx.HEADER().getText());
-        return v;
+        return s;
+    }
+
+    private String getVarName() {
+        return VAR_NAME + varCount++;
     }
 
     private void visitBody(DocumentContext ctx) {
@@ -107,15 +128,108 @@ public class DocumentConverter extends DocumentParserBaseVisitor<Object> {
 
     private void visitPrefixCall(ContentContext ctx) {
         if (ctx.prefixCall() != null) {
+            String methodName = "";
             for (TerminalNode h : ctx.prefixCall().HASH()) {
-                System.out.print(h.toString());
+                methodName += h;
             }
-            System.out.print(" ");
+            System.out.print(methodName + " ");
+
+            String parameter = "";
             for (TerminalNode w : ctx.prefixCall().sentence().WORD()) {
-                System.out.print(w + " ");
+                parameter += w + " ";
             }
-            System.out.println();
+            System.out.println(parameter);
+
+            boolean nested = isNested(methodName);
+            Method m = findOriginalMethod(methodName, nested);
+
+            TypeReference type = m.returnTypeReference();
+            String name = getVarName();
+            List<FormalParameter> parameters = new ArrayList<>();
+            parameters.add(new FormalParameter(parameter, ooFactory().createTypeReference("String")));
+            if (isNested(m)) {
+                parameters.add(new FormalParameter("" + methodName.length(), ooFactory().createTypeReference("Integer")));
+            }
+
+            Expression expression = expressionFactory().createMethodCall(vars.peek().getName(), m.name(), parameters);
+            block.addStatement(ooFactory().createLocalVariable(type, name, expression));
+            vars.push(new Variable(name, type));
         }
+    }
+
+    private Method findOriginalMethod(String methodName, boolean nested) {
+        Method originalMethod = null;
+        while (originalMethod == null) {
+            if (vars.isEmpty()) {
+                throw new IllegalArgumentException("The method " + methodName + " could not be found.");
+            }
+
+            Variable var = vars.peek();
+            List<Method> methods = getMethods(var.getType());
+            if (nested) {
+                List<Method> nestedMethods = methods.stream().filter(this::isNested).collect(Collectors.toList());
+                originalMethod = findMethod(methodName.charAt(0) + "", nestedMethods);
+            }
+
+            // Did not find it as nested method, try the other methods
+            if (originalMethod == null) {
+                originalMethod = findMethod(methodName, methods);
+            }
+
+            // Did not find the method, try on the other part of the tree
+            if (originalMethod == null) {
+                vars.pop();
+            }
+        }
+
+        return originalMethod;
+    }
+
+    private boolean isNested(Method m) {
+        return hasModifier(m, new Nested());
+    }
+
+    private boolean hasModifier(Method method, Modifier has) {
+        for (Modifier m : method.modifiers()) {
+            if (m.getClass().isInstance(has)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Method findMethod(String methodName, List<Method> methods) {
+        for (Method m : methods) {
+            if (m.name().equals(methodName)) {
+                return (Method) m.clone();
+            }
+        }
+
+        return null;
+    }
+
+    private List<Method> getMethods(TypeReference type) {
+        List<Method> result = new ArrayList<>();
+        try {
+            result.addAll(view.findType(type.toString()).descendants(Method.class));
+        } catch (LookupException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /**
+     * Checks if a method could be a nested method
+     *
+     * @param method the name of the method to test
+     * @return if the method might be a nested method
+     */
+    private boolean isNested(String method) {
+        Pattern p = Pattern.compile("^" + method.charAt(0) + "+$");
+        Matcher m = p.matcher(method);
+        return m.matches();
     }
 
     private void visitPostFixCall(ContentContext ctx) {
