e6b499e9c3d208e3ef437fca7bcc771b4d14de25
Titouan Vervack <tivervac@gmail.com>
2015-11-17 00:05:33 +0100

    Version bump

--- neio/src/main/java/be/ugent/neio/util/Constants.java
+++ neio/src/main/java/be/ugent/neio/util/Constants.java
@@ -6,7 +6,7 @@ package be.ugent.neio.util;
 public class Constants {
     public static final String LANG_NAME = "Neio";
     public static final String EXTENSION = "no";
-    public static final String VERSION = "0.5";
+    public static final String VERSION = "0.6";
     public static final String NEIO_HOME = "../examples/" + VERSION + "/neio";
     public static final String AUTO_GEN_DIR = "./gen";
 }
