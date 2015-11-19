ee3dfdb76c275e5413c521bf84363137456cf263
Titouan Vervack <tivervac@gmail.com>
2015-11-19 20:55:05 +0100

    Allow creation of TextDocument

--- neio/src/main/java/be/ugent/neio/model/document/TextDocument.java
+++ neio/src/main/java/be/ugent/neio/model/document/TextDocument.java
@@ -1,13 +1,41 @@
 package be.ugent.neio.model.document;
 
+import be.kuleuven.cs.distrinet.jnome.workspace.JavaView;
 import org.aikodi.chameleon.core.document.Document;
+import org.aikodi.chameleon.exception.ChameleonProgrammerException;
 import org.aikodi.chameleon.oo.statement.Block;
+import org.aikodi.chameleon.workspace.FakeDocumentLoader;
+import org.aikodi.chameleon.workspace.FakeDocumentScanner;
+import org.aikodi.chameleon.workspace.ProjectException;
+import org.aikodi.chameleon.workspace.View;
+import org.aikodi.contract.Contracts;
 
 /**
  * @author Titouan Vervack
  */
 public class TextDocument extends Document {
+
     private Block block;
+
+    /**
+     * @param view The view of this document
+     * @see {@link #cloneTo(View) cloneTo}
+     */
+    public TextDocument(JavaView view, Block block) {
+        this.block = block;
+        Contracts.notNull(view);
+
+        FakeDocumentScanner pl = new FakeDocumentScanner();
+        new FakeDocumentLoader(this, pl);
+
+        try {
+            view.addSource(pl);
+        } catch (ProjectException e) {
+            throw new ChameleonProgrammerException(e);
+        }
+
+        activate();
+    }
 
     public Block getBlock() {
         return block;
