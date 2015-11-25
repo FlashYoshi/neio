9b25d3184dcaf9c41d9e8ac170beeaf549f4911c
Titouan Vervack <tivervac@gmail.com>
2015-11-25 06:08:20 +0100

    Typechecking

--- neio/src/main/java/be/ugent/neio/translate/Java8Generator.java
+++ neio/src/main/java/be/ugent/neio/translate/Java8Generator.java
@@ -4,16 +4,21 @@ import be.ugent.neio.industry.NeioExpressionFactory;
 import be.ugent.neio.industry.NeioFactory;
 import be.ugent.neio.language.Neio;
 import be.ugent.neio.model.document.TextDocument;
+import be.ugent.neio.model.modifier.Nested;
+import be.ugent.neio.util.Variable;
 import org.aikodi.chameleon.core.lookup.LookupException;
-import org.aikodi.chameleon.oo.expression.Expression;
+import org.aikodi.chameleon.core.lookup.NameSelector;
+import org.aikodi.chameleon.core.lookup.SelectionResult;
 import org.aikodi.chameleon.oo.expression.ExpressionFactory;
 import org.aikodi.chameleon.oo.expression.MethodInvocation;
 import org.aikodi.chameleon.oo.method.ExpressionImplementation;
+import org.aikodi.chameleon.oo.method.Method;
 import org.aikodi.chameleon.oo.method.RegularImplementation;
 import org.aikodi.chameleon.oo.plugin.ObjectOrientedFactory;
 import org.aikodi.chameleon.oo.statement.Block;
 import org.aikodi.chameleon.oo.statement.Statement;
 import org.aikodi.chameleon.oo.type.Type;
+import org.aikodi.chameleon.support.member.simplename.method.NormalMethod;
 import org.aikodi.chameleon.support.statement.ReturnStatement;
 
 import java.util.ArrayList;
@@ -44,6 +49,7 @@ public class Java8Generator extends AbstractJava8Generator {
 
     private void replaceMethodChain(TextDocument neioDocument) throws LookupException {
         List<Statement> newStatements = new ArrayList<>();
+        Stack<Variable> variables = new Stack<>();
 
         for (Statement methodChain : neioDocument.getBlock().statements()) {
             Stack<MethodInvocation> callStack = new Stack<>();
@@ -57,23 +63,22 @@ public class Java8Generator extends AbstractJava8Generator {
             // Push the new call on the stack
             callStack.push(nmi);
 
-            boolean first = true;
             while (!callStack.isEmpty()) {
                 MethodInvocation call = callStack.pop();
                 Type type = call.getType();
+                Method method = (Method) call.getElement();
+                String methodName = method.name();
+                String prefix = getPrefix(type, methodName, variables);
 
-                Expression expression;
-                if (first) {
-                    call.setTarget(null);
-                    // If this is the first expression then it has to be a new call
-                    expression = eFactory().createNewExpression(type.name());
-                    first = false;
-                } else {
-                    // TODO: now set the right var as prefix (= target)
-                    expression = call;
+                call.setTarget(eFactory().createNameExpression(prefix));
+                // Add nesting level if this is a nested method
+                if (method.hasModifier(new Nested())) {
+                    call.addArgument(oFactory().createIntegerLiteral(String.valueOf(methodName.length())));
                 }
 
-                Statement s = oFactory().createLocalVariable(neio.createTypeReference(type.name()), getVarName(), expression);
+                String varName = getVarName();
+                Statement s = oFactory().createLocalVariable(neio.createTypeReference(type.name()), varName, call);
+                variables.push(new Variable(type.name(), varName));
                 newStatements.add(s);
             }
         }
@@ -81,6 +86,26 @@ public class Java8Generator extends AbstractJava8Generator {
         Block block = new Block();
         block.addStatements(newStatements);
         neioDocument.setBlock(block);
+    }
+
+    private String getPrefix(Type type, String methodName, Stack<Variable> variables) throws LookupException {
+        List<? extends SelectionResult> declarations = type.declarations(new NameSelector<NormalMethod>(NormalMethod.class) {
+            @Override
+            public String name() {
+                return methodName;
+            }
+        });
+
+        if (!declarations.isEmpty()) {
+            while (!variables.isEmpty()) {
+                Variable var = variables.pop();
+                if (var.getTypeName().equals(type.name())) {
+                    return var.getVarName();
+                }
+            }
+        }
+
+        return null;
     }
 
     private String getVarName() {
