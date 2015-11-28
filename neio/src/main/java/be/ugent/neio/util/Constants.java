e717829677ed4cb2f925029e75d33411e4e04cd7
Titouan Vervack <tivervac@gmail.com>
2015-11-28 23:53:01 +0100

    Some constants changes

--- neio/src/main/java/be/ugent/neio/util/Constants.java
+++ neio/src/main/java/be/ugent/neio/util/Constants.java
@@ -6,6 +6,7 @@ package be.ugent.neio.util;
 public class Constants {
     public static final String EXTENSION = "no";
     public static final String VERSION = "0.6";
-    public static final String NEIO_HOME = "../examples/" + VERSION + "/neio";
+    public static final String BASE = "../examples/" + VERSION;
+    public static final String NEIO_HOME = BASE + "/neio";
     public static final String AUTO_GEN_DIR = "./gen";
 }
